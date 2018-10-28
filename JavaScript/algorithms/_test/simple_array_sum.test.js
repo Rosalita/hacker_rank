d5cb5b079ffc6edda3c87f0840acde9a5fcf4e1d
Rosie <enchantrix@gmail.com>
2018-10-28 09:19:25 +0000

    solved simple_array_sum in JavaScript

---
+++ JavaScript/algorithms/_test/simple_array_sum.test.js
@@ -0,0 +1,13 @@
+const simpleArraySum = require("../simple_array_sum.js").simpleArraySum
+
+test('simpleArraySum can add an array of two positive numbers', () => {
+    const nums = [1, 2]
+    expect(simpleArraySum(nums)).toEqual(3)
+  })
+
+  test('simpleArraySum if given strings will add them to 0', () => {
+    const nums = ["fish", "chips"]
+    expect(simpleArraySum(nums)).toEqual("0fishchips")
+  })
+
+  
---
+++ JavaScript/algorithms/simple_array_sum.js
@@ -0,0 +1,15 @@
+function simpleArraySum(ar) {
+
+    let sum = ar.reduce(add, 0); 
+    // The reduce method reduces an array to a single value
+    // array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+    // reduce takes in a function and an initial value
+
+    function add(a, b){
+        return a + b
+    }
+
+    return sum
+}
+
+module.exports.simpleArraySum = simpleArraySum;
